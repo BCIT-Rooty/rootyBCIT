b8a9bf4ebae3fab6e58f4509af3c123aef79dae5
Sohrab Radmehr <Sohrab.ram95@hotmail.com>
2022-10-23 01:34:40 -0700

    the category is finished

--- rooty-next/components/dbFunctions/databaseFunctions.js
+++ rooty-next/components/dbFunctions/databaseFunctions.js
@@ -1,4 +1,49 @@
 import { prisma } from "../../server/db/client";
 
+export async function createPost({
+  photoUrl,
+  whatIsTheCategoryOfThisPost,
+  keywords,
+  title,
+  description,
+  isBarter,
+  price,
+  isNegotiableActive,
+  count,
+}) {
+  const category = await prisma.category.findUnique({
+    where: {
+      categoryName: whatIsTheCategoryOfThisPost,
+    },
+  });
 
+  console.log(category);
 
+  const post = await prisma.post.create({
+    data: {
+      title: title,
+      categoryId: category.categoryId,
+      // keywords: figure this out too
+      description: description,
+      barterInformation: isBarter,
+      price: parseInt(price),
+      negotiable: isNegotiableActive,
+      count: count,
+    },
+  });
+  const photo = await prisma.photo.create({
+    data: {
+      userPhotoUrl: photoUrl,
+      postId: post.postId,
+    },
+  });
+}
+
+export async function createPhoto(photoUrl) {
+  const photo = await prisma.photo.create({
+    data: {
+      userPhotoUrl: photoUrl,
+    },
+  });
+  console.log(photo);
+}
