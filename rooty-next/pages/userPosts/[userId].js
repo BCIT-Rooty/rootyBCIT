2e49165ebb82c1538da08896de073e2d21f33d7a
Sohrab Radmehr <Sohrab.ram95@hotmail.com>
2022-10-26 01:17:25 -0700

    Update [userId].js

--- rooty-next/pages/userPosts/[userId].js
+++ rooty-next/pages/userPosts/[userId].js
@@ -1,4 +1,4 @@
-import Item from '../../components/item';
+import Item from '../../components/Item';
 // import { getItemsForUser } from '../../../server/database.js'
 import { prisma } from '../../server/db/client';
 
