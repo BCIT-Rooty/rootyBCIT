b6024d141feb531d5f6dad04273a8e95f9a1df08
Sohrab Radmehr <Sohrab.ram95@hotmail.com>
2022-11-09 00:38:01 -0800

    Update [chatRoomId].jsx

--- rooty-next/pages/chat/[chatRoomId].jsx
+++ rooty-next/pages/chat/[chatRoomId].jsx
@@ -11,32 +11,32 @@ export default function ACertainChatRoom(props) {
   const [message, setMessage] = useState("");
   const socket = io();
   const [chats, setChats] = useState([]);
-  const [userId, setUserId] = useState("")
+  const [userId, setUserId] = useState("");
   var userIdGlobal;
-
-
 
   useEffect(() => {
     userIdGlobal = Math.floor(Math.random() * 10000) + 1;
+    let oldPosts;
     fetch("/api/socketio").finally(() => {
       socket.on("connect", () => {
         joinRoom(theChatRoomId);
-        console.log(socket.id)
-        setUserId(socket.id)
+        console.log(socket.id);
+        setUserId(socket.id);
+        (async function yo() {
+          await axios
+            .post("/api/allChat", { userId: props.theId })
+            .then((res) => {
+              console.log(res);
+              oldPosts = res.data.map((m) => <MyMessage text={m.content} />);
+            });
+        })();
         // The userID doesn't exits now because that we don't have users now!
         socket.on("receive-message", (message, userId, frontId) => {
           if (frontId == socket.id) {
-            return
+            return;
           } else {
             console.log("This is someone else", message, userId, frontId);
-            ;(async function yo()  {
-              await axios.post("/api/allChat", {userId: props.theId}).then((res) => {
-                console.log(res)
-                const oldPosts = (res.data).map(m => <MyMessage text={m.content} />)
-                setChats([...oldPosts, <MyMessage text={message} />])
-              })
-            })()
-            
+            setChats([...oldPosts, <MyMessage text={message} />]);
           }
         });
       });
@@ -45,16 +45,15 @@ export default function ACertainChatRoom(props) {
       socket.emit("avoid-duplicate");
     };
   }, []);
-  
+
   useEffect(() => {
-    ;(async function yo()  {
-      await axios.post("/api/allChat", {userId: socket.id}).then((res) => {
-        console.log(res)
-        const oldPosts = (res.data).map(m => <MyMessage text={m.content} />)
-        setChats(oldPosts)
-      })
-    })()
-  }, [])
+    (async function yo() {
+      await axios.post("/api/allChat", { userId: socket.id }).then((res) => {
+        const oldPosts = res.data.map((m) => <MyMessage text={m.content} />);
+        setChats(oldPosts);
+      });
+    })();
+  }, []);
 
   function joinRoom(room) {
     console.log("Joined room", room);
