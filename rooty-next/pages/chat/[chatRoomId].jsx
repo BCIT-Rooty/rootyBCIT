589ebb48f055240fa28e63388bfcda11c23381e2
Sohrab Radmehr <Sohrab.ram95@hotmail.com>
2022-11-15 19:26:57 -0800

    Update [chatRoomId].jsx

--- rooty-next/pages/chat/[chatRoomId].jsx
+++ rooty-next/pages/chat/[chatRoomId].jsx
@@ -6,11 +6,11 @@ import axios from "axios";
 import MyMessage from "../../components/chat/myMessage";
 import NotMyMessage from "../../components/chat/notMyMessage";
 import { FlexBox, Wrapper } from "../../styles/globals";
+const socket = io.connect("http://localhost:3001");
 
 export default function ACertainChatRoom(props) {
   const theChatRoomId = props.theId;
   const [message, setMessage] = useState("");
-  const socket = io();
   const [chats, setChats] = useState([]);
   const [userId, setUserId] = useState("");
   var userIdGlobal;
@@ -18,37 +18,45 @@ export default function ACertainChatRoom(props) {
 
   useEffect(() => {
     userIdGlobal = Math.floor(Math.random() * 10000) + 1;
-    fetch('http://localhost:8000/').finally(() => {
-      const socket = io()
-      socket.on('connect', () => {
-        console.log('connect')
-        socket.emit('hello')
-      })
-    })
-
+    socket.emit("join_room", props.theId);
+    return () => {
+      socket.emit("avoid-duplicate", {room : props.theId});
+    }
   }, []);
 
+  useEffect(() => {
+    (async function yo() {
+      await axios.post("/api/allChat", { userId: props.theId }).then((res) => {
+        const oldPosts = res.data.map((m) => <MyMessage key={m.messageId} text={m.content} />);
+        setChats(oldPosts);
+      });
+    })();
+  }, []);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      getNewData()
+    });
+  }, [socket]);
 
   function getNewData() {
     (async function yo() {
-      await axios
-        .post("/api/allChat", { userId: props.theId })
-        .then((res) => {
-          console.log(res);
-          oldPosts = res.data.map((m) => <MyMessage text={m.content} />);
-          setChats(oldPosts);
-        });
+      await axios.post("/api/allChat", { userId: props.theId }).then((res) => {
+        // console.log(res);
+        oldPosts = res.data.map((m) => <MyMessage key={m.messageId} text={m.content} />);
+        setChats(oldPosts);
+      });
     })();
   }
 
   function joinRoom(room) {
     console.log("Joined room", room);
-    socket.emit("join-room", room, userIdGlobal);
+    socket.emit("join_room", room);
   }
 
   async function sendTextToTheBackEnd(inputText, room) {
-    socket.emit("send-text", inputText, room, userId);
-    setChats([...chats, <MyMessage text={inputText} />]);
+    socket.emit("send_message", inputText, room, userId);
+    // setChats([...chats, <MyMessage text={inputText} />]);
   }
 
   function handleSendButton(e) {
@@ -65,15 +73,29 @@ export default function ACertainChatRoom(props) {
 
   return (
     <>
-      <Wrapper justifyContent="flex-start" alignItems="flex-start" height="fit-content" padding="0 0 80px 0">
-        <FlexBox dir="column" justifyContent="flex-end" alignItems="flex-start" width="100%" height="fit-content" >
-      <FlexBox dir="column" width="100%">{chats.map((m) => m)}</FlexBox>
-      <ChatNavBar position="fixed"
-        value={message}
-        onChangingTheTextForChat={handleChangeText}
-        onSubmitButtonClicked={handleSendButton}
-      />
-      </FlexBox>
+      <Wrapper
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        height="fit-content"
+        padding="0 0 80px 0"
+      >
+        <FlexBox
+          dir="column"
+          justifyContent="flex-end"
+          alignItems="flex-start"
+          width="100%"
+          height="fit-content"
+        >
+          <FlexBox dir="column" width="100%">
+            {chats.map((m) => m)}
+          </FlexBox>
+          <ChatNavBar
+            position="fixed"
+            value={message}
+            onChangingTheTextForChat={handleChangeText}
+            onSubmitButtonClicked={handleSendButton}
+          />
+        </FlexBox>
       </Wrapper>
     </>
   );
