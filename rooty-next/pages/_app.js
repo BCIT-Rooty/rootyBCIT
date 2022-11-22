0da005b74d487306fb22cb55075b70ebc5be9bb5
Sohrab Radmehr <Sohrab.ram95@hotmail.com>
2022-11-22 01:34:21 -0800

    Update _app.js

--- rooty-next/pages/_app.js
+++ rooty-next/pages/_app.js
@@ -1,46 +1,71 @@
-import '../styles/globals.css'
-import NavBar from '../components/navbar/navbar'
-import 'semantic-ui-css/semantic.min.css'
-import { useRouter } from 'next/router'
-import Head from 'next/head'
-import { useState, useEffect } from 'react'
+import "../styles/globals.css";
+import NavBar from "../components/navbar/navbar";
+import "semantic-ui-css/semantic.min.css";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { useState, useEffect } from "react";
 function MyApp({ Component, pageProps }) {
-
-  const [routePath, setRoutePath] = useState("")
-  const [routesWeDoNotWant, setRoutesWeDoNotWant] = useState([])
-  const [shouldTheRouteLoad, setShouldTheRouteLoad] = useState(true)
-  const router = useRouter()
-
-  useEffect(() => {
-    const thisPath = router.asPath
-    setRoutePath(thisPath)
-    routesWeDoNotWant.map(m => {
-      if (m == thisPath) {
-        setShouldTheRouteLoad(false)
-        return
-      } else {
-        return
-      }
-    })
-  }, []) 
-  
-
-
+  const [routesWeDoNotWant, setRoutesWeDoNotWant] = useState(["/chat/*"]);
+  const [shouldTheRouteLoad, setShouldTheRouteLoad] = useState(true);
+  const router = useRouter();
 
   // console.log(router)
   return (
     <>
-    <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/> 
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/> 
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"/>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-    {/* <AnimatePresence exitBeforeEnter> */}
-      {shouldTheRouteLoad? <NavBar route={router.route} />: <></>}     
+      {/* <AnimatePresence exitBeforeEnter> */}
+
+      {routesWeDoNotWant.map((m) => {
+        const lengthOfPath = m.length;
+        if (m[lengthOfPath - 1] == "*") {
+          if (router.asPath.startsWith(m.slice(0, -1))) {
+            return <></>;
+          } else {
+            return <NavBar route={router.route} />;
+          }
+        } else if (m == router.asPath) {
+          return <></>;
+        } else {
+          return <NavBar route={router.route} />;
+        }
+      })}
+
       <Component {...pageProps} />
-    {/* </AnimatePresence> */}
+      {/* </AnimatePresence> */}
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+// If I wanted to make changes later
+
+// useEffect(() => {
+//   const thisPath = router.asPath
+//   routesWeDoNotWant.map(m => {
+//     const lengthOfPath = m.length
+//     if (m[lengthOfPath - 1] == "*") {
+//       // console.log(thisPath.startsWith(m.slice(0, -1)))
+//       if (thisPath.startsWith(m.slice(0, -1))) {
+//        setShouldTheRouteLoad(false)
+//        return
+//       } else {
+//         setShouldTheRouteLoad(true)
+//         return
+//       }
+//     } else if (m == thisPath) {
+//       setShouldTheRouteLoad(false)
+//       return
+//     } else {
+//       setShouldTheRouteLoad(true)
+//       return
+//     }
+//   })
+// }, [router.asPath])
