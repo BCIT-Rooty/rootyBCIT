2258521a1087e1914e95e0832429398a04de9b54
Sohrab Radmehr <Sohrab.ram95@hotmail.com>
2022-11-22 22:02:37 -0800

    searchBar fixed

--- rooty-next/components/chat/chatNavBar.js
+++ rooty-next/components/chat/chatNavBar.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import { grey, purple } from '@mui/material/colors';
+import InputFile from "../../components/inputs/index";
 
 
 export default function ChatNavBar({
@@ -19,7 +20,7 @@ position={position}
     }
     return(
     <FlexBox width="100%" justifyContent="space-around" padding="10px" topBorder="1px #EDEDED solid" top="93vh" position={position} zIndex="20" bgColor="white">
-        <AddIcon onClick={(e) => onSubmitButtonClicked(e)} fontSize="large" sx={{ color: grey[500] }}></AddIcon>
+       <AddIcon onClick={(e) => onSubmitButtonClicked(e)} fontSize="large" sx={{ color: grey[500] }} />
         <FlexBox>
             <Input value={value} onChangingTheText={newFunction} placeholder="Type a Message Here" width="75vw" />
         </FlexBox>
--- rooty-next/pages/categories/index.js
+++ rooty-next/pages/categories/index.js
@@ -62,7 +62,6 @@ export default function Categories({jsonCategories}) {
               onResultSelect={(e, data) => 
                 dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title })} />
                 {/* delete the input if anything wrong */}
-          <Input bgImage="/icons8-search-48.png" bgSize="30px" type="email" placeholder='Search services' margin="0px 20px 0px 20px" padding='0 0 0 55px' width="90vw" maxWidth="900px" justifyContent="flex-start"></Input>
           </FlexBox>
           <FlexBox flexWrap="wrap" filter="drop-shadow(0px 5px 6px rgba(0, 0, 0, 0.2))">
             {getCategories(categories)}
